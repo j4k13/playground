c3c660ea7e762b81b3c1bc600851490b9e712b41
j4k13 <namlesscrusader@gmail.com>
2016-04-12 14:36:27 -0400

    Started playground repo

---
+++ tryStrip.php
@@ -0,0 +1,52 @@
+<?php
+	//make array or list of people objects
+
+	//regex pulls names loop
+
+	//regex to grab info for each object
+	//<span something 3 >(something for string|something for number stuff for in between </span>
+	//loop for objects
+		//loop for 8 attributes
+
+	//class to store the info into for people
+	class person
+	{
+		$traits[0];
+		function setName($input)
+		{
+			$traits[0] = $input;
+		}
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+		function setName($input)
+                {
+                        $traits[0] = $input;
+                }
+	}
+?>
