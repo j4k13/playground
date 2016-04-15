0f252ec699d08e3e5d11eac43701c431ec734177
j4k13 <namlesscrusader@gmail.com>
2016-04-15 00:55:41 -0400

    Its not working at all

--- tryStrip.php
+++ tryStrip.php
@@ -7,23 +7,7 @@
 	//get file string for parse 
 	$namestring = file_get_contents("persons.txt");
 	//parse with regex don't include ":" to eliminate catergories
-	preg_match_all("#<span class=\"c3 c4\">(.*?)</span>#",$namestring,$namematch );
-	var_dump(implode($namematch[1]));
-	//preg_match_all("#\D{3}#",$name2,$namematch2);
-	//$output = fopen("output.txt","w");
-	//var_dump();
-	//foreach($namematch2 as $line)
-	//	var_dump($line);
-	//names loop
-	//make array for matches to info
-	//regex to grab info for each object
-	//<span something 3 >(something for string|\d{2} stuff for in between </span>
-	//loop for objects
-		//loop for 8 attributes
-
-	//class to store the info into for people
-	class person
-	{
-		}
-	
+	preg_match_all("#Contact(.*?): (.*?)</span>#",$namestring,$namematch);
+	var_dump($namematch[1]);
+		
 ?>
