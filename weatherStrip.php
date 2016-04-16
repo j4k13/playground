46597a8d34b87a4d4791aff1f5d3b23305754e92
j4k13 <namlesscrusader@gmail.com>
2016-04-16 14:52:47 -0400

    Added stripping of the events for weather

--- weatherStrip.php
+++ weatherStrip.php
@@ -1,9 +1,11 @@
 <?php
 	date_default_timezone_set('America/New_York');
-        $trafficFile = file_get_contents("weather.xml");
-        $trafficMatch[] = 0;
-        preg_match_all("#<cap:areaDesc>(.*?)</cap:areaDesc>#",$trafficFile,$trafficMatch);
-        var_dump($trafficMatch[1]);
-	
+        $whereFile = file_get_contents("weather.xml");
+        $whereMatch[] = 0;
+        preg_match_all("#<cap:areaDesc>(.*?)</cap:areaDesc>#",$whereFile,$whereMatch);
+        var_dump($whereMatch[1]);
+	$whatMatch[] = 0;
+	preg_match_all("#<summary>(.*?)</summary>#",$whereFile,$whatMatch);
+	var_dump($whatMatch[1]);
 
 ?>
