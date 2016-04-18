b4ef1154861afb78cd3731cf9a965eca62d536b9
j4k13 <namlesscrusader@gmail.com>
2016-04-18 09:07:50 -0400

    Things

---
+++ missingpersons.php
@@ -0,0 +1,13 @@
+<?php
+	
+	date_default_timezone_set('America/New_York');
+	//make array or list of people objects
+	//make array of matches for names
+	$namematch[] = 0;
+	//get file string for parse 
+	$namestring = file_get_contents("persons.txt");
+	//parse with regex don't include ":" to eliminate catergories
+	preg_match_all("#Contact(.*?): (.*?)</span>#",$namestring,$namematch);
+	var_dump($namematch[1]);
+		
+?>
