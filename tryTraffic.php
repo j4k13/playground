83d81824ad452af1af2edd80f1dc4ff4b633deee
j4k13 <namlesscrusader@gmail.com>
2016-04-20 06:34:55 -0400

    somehow magically works

--- tryTraffic.php
+++ tryTraffic.php
@@ -1,9 +1,9 @@
 <?php
 	date_default_timezone_set('America/New_York');
-	$trafficFile = file_get_contents("-\ Mobile\ Feeds.html");
+	$trafficFile = file_get_contents("traffic.txt");
 	//http://www.511virginia.org/mobile/?menu_id=twitter
 	//var_dump($trafficFile);
 	$trafficMatch[] = 0;
-	preg_match_all("#<p style=\"font-weight:bold;\">(.*?)</p>#",$trafficFile,$trafficMatch);
+	preg_match_all("#<li>Cleared: (.*); #",$trafficFile,$trafficMatch);
 	var_dump($trafficMatch[1]);
 ?>
