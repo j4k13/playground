759816793dbeb785e997c7fadefc1aae9a4f3e18
j4k13 <namlesscrusader@gmail.com>
2016-04-20 08:43:40 -0400

    almost parsing

--- tryTraffic.php
+++ tryTraffic.php
@@ -4,6 +4,27 @@
 	//http://www.511virginia.org/mobile/?menu_id=twitter
 	//var_dump($trafficFile);
 	$trafficMatch[] = 0;
-	preg_match_all("#<li>Cleared: (.*); #",$trafficFile,$trafficMatch);
-	var_dump($trafficMatch[1]);
+	preg_match_all("#<li>Accident: (.*); #",$trafficFile,$trafficMatch);
+	//var_dump($trafficMatch[1]);
+	$theWhere[] = 0;
+	$revisedWhere[] = 0;
+	$iterator = 0;
+	foreach($trafficMatch[1] as $line)
+	{
+	preg_match_all("#in (.*?) .#",$line,$theWhere[$iterator]);
+	//var_dump($theWhere[1]);
+	$iterator++;
+	}
+	//var_dump($theWhere);
+	$iterator = 0;
+	foreach($theWhere as $index)
+	{
+		$revisedWhere[$iterator] = $index[1];
+	}
+	var_dump($revisedWhere);
+	for($iterator = 0 ; $iterator < sizeOf($theWhere) ; $iterator++)
+	{
+		//$line = rtrim($line[1],".");
+	}
+	//var_dump($theWhere);
 ?>
