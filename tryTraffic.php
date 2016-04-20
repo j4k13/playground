0f6edb228856db4aeba314ee45ef4bc61239ffb2
j4k13 <namlesscrusader@gmail.com>
2016-04-20 00:21:51 -0400

    Minor changes

--- tryTraffic.php
+++ tryTraffic.php
@@ -1,6 +1,7 @@
 <?php
 	date_default_timezone_set('America/New_York');
-	$trafficFile = file_get_contents("511.html");
+	$trafficFile = file_get_contents("-\ Mobile\ Feeds.html");
+	//http://www.511virginia.org/mobile/?menu_id=twitter
 	//var_dump($trafficFile);
 	$trafficMatch[] = 0;
 	preg_match_all("#<p style=\"font-weight:bold;\">(.*?)</p>#",$trafficFile,$trafficMatch);
